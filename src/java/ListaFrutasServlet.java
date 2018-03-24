5919fa4e1a75448423214e3fd7ff38435b13002c
ice <ice@lab03m25.nrc.ice.ufjf.br>
2018-03-23 21:12:45 -0300

    Crição do Projeto

---
+++ src/java/ListaFrutasServlet.java
@@ -0,0 +1,7 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/ListaFrutasServlet.html"})
+public class ListaFrutasServlet extends HttpServlet {
+
+}
